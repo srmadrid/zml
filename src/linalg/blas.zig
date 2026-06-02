f7cea40ddaaf24803b85fc7938d3221525839c50
srmadrid <127796005+srmadrid@users.noreply.github.com>
2026-06-02 11:08:59 +0200

    Removed rotg, rotm and rotmg

--- src/linalg/blas.zig
+++ src/linalg/blas.zig
@@ -10,9 +10,6 @@ pub const dotc = @import("blas/level1/dotc.zig").dotc;
 pub const Nrm2 = @import("blas/level1/nrm2.zig").Nrm2;
 pub const nrm2 = @import("blas/level1/nrm2.zig").nrm2;
 pub const rot = @import("blas/level1/rot.zig").rot; // Untested and unbenchmarked
-// pub const rotg = @import("blas/level1/rotg.zig").rotg;
-// pub const rotm = @import("blas/level1/rotm.zig").rotm;
-// pub const rotmg = @import("blas/level1/rotmg.zig").rotmg;
 pub const scal = @import("blas/level1/scal.zig").scal;
 pub const swap = @import("blas/level1/swap.zig").swap;
 pub const iamax = @import("blas/level1/iamax.zig").iamax;
@@ -28,7 +25,6 @@ pub const her = @import("blas/level2/her.zig").her;
 pub const symv = @import("blas/level2/symv.zig").symv;
 pub const syr = @import("blas/level2/syr.zig").syr;
 // pub const syr2 = @import("blas/level2/syr2.zig").syr2;
-// pub const tpsv = @import("blas/level2/tpsv.zig").tpsv;
 // pub const trmv = @import("blas/level2/trmv.zig").trmv;
 // pub const trsv = @import("blas/level2/trsv.zig").trsv;
 
